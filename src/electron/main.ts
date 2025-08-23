7e1de2b3f4c0914d391907df92f4455bd8d42c7e
kashyaptan <kashyaptan2004@gmail.com>
2025-08-23 01:36:33 -0400

    Made app window transparent and dragable

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -3,7 +3,16 @@ import path from 'path';
 import {isDev} from './utils.js';
 
 app.on('ready', ()=>{
-    const mainWindow = new BrowserWindow({});
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 500,
+        minWidth: 400,
+        minHeight: 300,
+        title: 'Clueless',
+        frame: false,
+        transparent: true,
+        resizable: true,
+    });
     if (isDev()){
         mainWindow.loadURL('http://localhost:5123');
     }
